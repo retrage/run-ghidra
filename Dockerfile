a53ae7e152477511b831ea565d78d8f7e7dae5cd
Akira Moroo <retrage01@gmail.com>
2022-06-12 19:12:14 +0900

    build: Add `plugins` input
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM ghcr.io/blacktop/ghidra:latest
 
+# Install dependencies
+RUN apt-get update \
+  && apt-get -yq upgrade \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -yq \
+    wget \
+    unzip \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@
 set -eux
 
 readarray -t -d '' args < <(xargs printf '%s\0' <<<"$INPUT_ARGS")
+readarray -t -d '' plugins < <(xargs printf '%s\0' <<<"$INPUT_PLUGINS")
+
+
+
+# Download and install Ghidra plugins
+for plugin in "${plugins[@]}"; do
+  wget -P /ghidra/Ghidra/Extensions "$plugin"
+  unzip -q -d /ghidra/Ghidra/Extensions "/ghidra/Ghidra/Extensions/$(basename "$plugin")"
+done
 
 mkdir -p "$INPUT_PROJECT_LOCATION"
 
