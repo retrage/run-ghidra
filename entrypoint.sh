812061b159aa57d21c8b358679ff5d6d865b6c86
Akira Moroo <retrage01@gmail.com>
2022-06-12 20:26:09 +0900

    [WIP] build: Cleanup action configs
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
+if [[ -n "$INPUT_PLUGINS" ]]; then
+  echo "::group::Download and install Ghidra plugins"
+  readarray -t -d '' plugins < <(xargs printf '%s\0' <<<"$INPUT_PLUGINS")
+  for plugin in "${plugins[@]}"; do
+    wget -q -P /ghidra/Ghidra/Extensions "$plugin"
+    unzip -q -d /ghidra/Ghidra/Extensions "/ghidra/Ghidra/Extensions/$(basename "$plugin")"
+  done
+  echo "::endgroup::"
+fi
+
+echo "::group::Run Ghidra"
+if [[ -n "$INPUT_LOG" ]]; then
+  log_args=("-log" "$INPUT_LOG")
+else
+  log_args=()
+fi
 readarray -t -d '' args < <(xargs printf '%s\0' <<<"$INPUT_ARGS")
-readarray -t -d '' plugins < <(xargs printf '%s\0' <<<"$INPUT_PLUGINS")
-
-
-
-# Download and install Ghidra plugins
-for plugin in "${plugins[@]}"; do
-  wget -P /ghidra/Ghidra/Extensions "$plugin"
-  unzip -q -d /ghidra/Ghidra/Extensions "/ghidra/Ghidra/Extensions/$(basename "$plugin")"
-done
-
 mkdir -p "$INPUT_PROJECT_LOCATION"
-
 /ghidra/support/analyzeHeadless \
   "$INPUT_PROJECT_LOCATION" \
   "$INPUT_PROJECT_NAME" \
   -import "$INPUT_IMPORT" \
-  -log "$INPUT_LOG" \
+  "${log_args[@]}" \
   "${args[@]}"
+echo "::endgroup::"
 
 echo "::set-output name=log::$(cat "$INPUT_LOG")"
